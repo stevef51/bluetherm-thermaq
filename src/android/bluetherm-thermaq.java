046bea2ac0ee60b6c5d77663f8c823dc7c161a48
Steve Fillingham <steve.fillingham@gmail.com>
2017-11-10 15:41:19 +1100

    dot dash underscore changes

--- src/android/bluetherm-thermaq.java
+++ src/android/bluetherm-thermaq.java
@@ -1,4 +1,4 @@
-package bluetherm-thermaq;
+package bluetherm_thermaq;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class bluetherm-thermaq extends CordovaPlugin {
+public class bluetherm_thermaq extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
